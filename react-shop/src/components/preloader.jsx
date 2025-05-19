a8872de957ae21d26cc1c20135a98e7565b9f7cc
zer0 <zer0@gmail.com>
2025-05-20 01:08:40 +0300

    Initial commit

---
+++ react-shop/src/components/preloader.jsx
@@ -0,0 +1,9 @@
+export function Preloader() {
+    return (
+      <div className="custom-preloader">
+        <div className="spinner"></div>
+        <p>Загрузка...</p>
+      </div>
+    );
+  }
+  
