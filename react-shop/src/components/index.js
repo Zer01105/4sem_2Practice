80dd5de589743f2e21162dbb251ef141b77ce236
zer0 <zer0@gmail.com>
2025-05-20 01:26:32 +0300

    Updates

--- react-shop/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { Header } from "./header";
-export { Shop } from "./shop";
-export { Footer } from "./footer";
--- react-shop/src/components/preloader.jsx
+++
@@ -1,9 +0,0 @@
-export function Preloader() {
-    return (
-      <div className="custom-preloader">
-        <div className="spinner"></div>
-        <p>Загрузка...</p>
-      </div>
-    );
-  }
-  
